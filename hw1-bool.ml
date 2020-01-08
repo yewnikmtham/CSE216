bd6a97f01f541855896be5142539125620cd8289
Michelle Tham <themichelletham@gmail.com>
2020-01-07 21:42:22 -0500

    Update hw1-bool.ml

--- hw1-bool.ml
+++ hw1-bool.ml
@@ -9,19 +9,23 @@ type bool_expr =
 	| Not of bool_expr
 	| And of bool_expr * bool_expr
 	| Or of bool_expr * bool_expr
+;;
 
-let rec truth_helper a a_val b b_val = function
+let rec truth_helper a b boola boolb exp =
+	match exp with
 	(* Bases on which of boolean operation is being used *)
-	| Lit x -> if x = a then a_val
-				else if x = b then b_val
-				else failwith "There is an invalid variable."
-	| Not e -> not(truth_helper a a_val b b_val e)
-	| And(e1, e2) -> (truth_helper a a_val b b_val e1) && (truth_helper a a_val b b_val e2)
-	| Or(e1, e2) -> (truth_helper a a_val b b_val e1) || (truth_helper a a_val b b_val e2)
+	Lit checks -> (match check with
+		| x -> boola
+		| y -> boolb
+		| _ -> failwith "Invalid variable.")
+	| Not exp -> not(truth_helper a b boola boolb exp)
+	| And(e1, e2) -> (truth_helper a b boola boolb e1) && (truth_helper a b boola boolb e2)
+	| Or(e1, e2) -> (truth_helper a b boola boolb e1) || (truth_helper a b boola boolb e2)
+;;
 
 let truth_table a b exp = (* Truth Table and calls helper to evaluate boolean exp *)
-	[(true, true, truth_helper a true b true);
-		(true, false, truth_helper a true b false);
-		(false, true, truth_helper a false b true);
-		(false, false, truth_helper a false b false)]
+	[(true, true, truth_helper a b true true exp);
+		(true, false, truth_helper a b true false exp);
+		(false, true, truth_helper a b false true exp);
+		(false, false, truth_helper a b false false false false)]
 ;;
